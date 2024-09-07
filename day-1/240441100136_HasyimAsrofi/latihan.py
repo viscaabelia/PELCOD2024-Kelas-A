9accb34461cb83a875c91e13861f0d11fd9060f4
Rofyy <hasrofyy@gmail.com>
2024-09-07 12:17:11 +0700

    240441100136

---
+++ day-1/240441100136_HasyimAsrofi/latihan.py
@@ -0,0 +1,41 @@
+nama = "Hasyim Asrofi"
+nim  =240441100136
+ipk  =4.00
+mahasiswa = True
+
+print("nama saya adalah", nama)
+# ini adalah format natural (tipe data sama)
+print("nim saya adalah", nim)
+print("impian saya adalah", ipk)
+print("saya merupakan Mahasiswa", mahasiswa)
+
+# ini adalah string (tipe data diubah menjadi)
+print(f'Nim saya adalah {nim}')
+
+# program dinamis string
+# nama panjang = str(input("Nama saya adalah : ")
+
+# program dinamis integer
+# nilai matematika =int(input("Nilai saya adalah")
+
+nilai_matematika = 89
+nilai_biologi = 75
+nilai_kimia = 90
+nilai_fisika = 82
+
+penjumlahan= nilai_matematika + nilai_kimia 
+pengurangan= nilai_matematika - nilai_kimia
+perkalian = nilai_matematika  * nilai_kimia
+pembagian= nilai_matematika   / nilai_kimia
+
+# print(f' Hasil penjumlahan dari matematika dan kimia adalah)
+# print(f' Hasil penjumlahan dari matematika dan kimia adalah)
+
+usia_masuk_kuliah =int(input("Berapa usia anda"))
+lama_kuliah =int(input("Berapa lama anda kuliah (tahun)"))
+
+usia_saat_ini =usia_masuk_kuliah + lama_kuliah
+tahun_lulus = 2024 + lama_kuliah
+
+print(f'saya, bernama {nama} saya berusia') 
+print 
