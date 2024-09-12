055bc03ca335338ff2e08bb077df30a878f3e58d
viscaabelia <viscaabeliaa@gmail.com>
2024-09-12 16:04:28 +0700

    240441100116

---
+++ tugas/240441100116_ViscaAbelia/dialog.py
@@ -0,0 +1,15 @@
+karakter1 = "visca"
+karakter2 = "abel"
+
+print(f"{karakter1} : hai abel!")
+print(f"{karakter2} : hallo visca!")
+print(f"{karakter1} : bisa bantu aku mengerjakan tugas matematika tidak?")
+print(f"{karakter2} : oh iya bisa, apa soalnya?")
+
+angka1 = int(input(f"{karakter1} : angka pertama "))
+angka2 = int(input(f"{karakter1} : angka kedua "))
+total = angka1 + angka2
+print(f"{karakter2} : hasil dari penjumlahan angka1 dan angka2 adalah {total}")
+
+print(f"{karakter1} : terimakasih, abel!!")
+print(f"{karakter2} : sama-sama, visca")
